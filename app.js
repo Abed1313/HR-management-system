306e0878c1d79a9b6116891437ffd2e08d3306e3
Abed1313 <abedradwan84.5@gmail.com>
2024-03-27 22:15:55 +0000

    up2

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ class employee  {
 
 
 employee.prototype.calcSalary = function(){
-console.log("aaaaaaaaaaaaaaa")
+// console.log("aaaaaaaaaaaaaaa")
    
 
      var salaryRange={
@@ -27,7 +27,9 @@ console.log("aaaaaaaaaaaaaaa")
       
 
       function randomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min) + min);
+        let salTax= Math.floor(Math.random() * (max - min) + min);
+       return salTax -(salTax * 0.075);
+        
     }
 
   this.salary=randomNumber(reang.min, reang.max);
@@ -37,27 +39,29 @@ console.log("aaaaaaaaaaaaaaa")
 }
 
 
-
-employee.prototype.calcNetSalary=function(){
-
-}
-
-
 employee.prototype.renderInfo=function(){
 //DOM
 let root = document.getElementById("root");
 
+
+
 }
 
 
-let emplo= [
 
-    new employee(1, "abed ahmad", "Administration","Senior", "https://exampl.com/image1.png" ),
-    new employee(2,"farah samer", "Marketing","Junior","https://exampl.com/image2.png"),
-    new employee(3, "mohamad omer", "Development","Mid-Senior","https://exampl.com/image3.png"),
-    new employee(4,"taha abed","Finance","Junior","https://exampl.com/image4.png")
+    let emplo = [
+        new employee(1000, 'Ghazi Samer', 'Administration', 'Senior',"https://exampl.com/image1.png"),
+        new employee(1001, 'Lana Ali', 'Finance', 'Senior', "https://exampl.com/image2.png"),
+        new employee(1002, 'Tamara Ayoub', 'Marketing', 'Senior',"https://exampl.com/image3.png"),
+        new employee(1003, 'Safi Walid', 'Administration', 'Mid-Senior',"https://exampl.com/image4.png"),
+        new employee(1004, 'Omar Zaid', 'Development', 'Senior',"https://exampl.com/image5.png"),
+        new employee(1005, 'Rana Saleh', 'Development', 'Junior',"https://exampl.com/image6.png"),
+        new employee(1006, 'Hadi Ahmad', 'Finance', 'Mid-Senior',"https://exampl.com/image7.png")
+      ];
+
     
-];
+    
+
 
 
 for(let i=0;i< emplo.length; i++){
