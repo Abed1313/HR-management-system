d72dddc3e4b10776efcbebbc397fa120298358e8
Abed1313 <abedradwan84.5@gmail.com>
2024-03-31 22:22:02 +0000

    test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
-function employee (employeeID, fullName, department ,level, imageURL){
-        this.employeeID=employeeID;
+function employee ( fullName, department ,level, imageURL){
+        this.employeeID=this.uniqueNum();
         this.fullName=fullName;
         this.department=department;
         this.level=level;
@@ -52,25 +52,76 @@ document.body.insertBefore(root1,footer);
 
 }
 
+
+ employee.prototype.uniqueNum=function(max=9999,min=1000){
+   return Math.floor(Math.random() * (max - min) + min);
+}
+
+employee.prototype.card=function(){
+   let section = document.getElementById('Add');
+   let div1 =document.createElement('div');
+div1.classList.add('card');
+   section.append(div1);
+
+let img = document.createElement('img');
+img.setAttribute('src', this.imageURL);
+div1.append(img);
+let div2 =document.createElement('div');
+div2.classList.add('container');
+div1.append(div2);
+let h4 =document.createElement('h4');
+h4.textContent= this.fullName ;
+div2.append(h4);
+let h5 =document.createElement('h5');
+h5.textContent=` ${this.department} / ${this.level} / ${this.employeeID}`;
+div2.append(h5);
+
+}
+
+
+let formEle = document.getElementById("myForm");
+formEle.addEventListener("submit", submitEHandeler);
+
+function submitEHandeler(event){
+   event.prevrntDefault();
+
+   let fullName= event.target.name.value;
+   let addN = new employee(fullName, department ,level, imageURL);
+   
+   addN.renderInfo();
+}
+
+   
+
+
+
     
-     let emplo1=   new employee(1000, 'Ghazi Samer', 'Administration', 'Senior',"https://exampl.com/image1.png");
-     let emplo2= new employee(1001, 'Lana Ali', 'Finance', 'Senior', "https://exampl.com/image2.png");
-     let emplo3=  new employee(1002, 'Tamara Ayoub', 'Marketing', 'Senior',"https://exampl.com/image3.png");
-     let emplo4= new employee(1003, 'Safi Walid', 'Administration', 'Mid-Senior',"https://exampl.com/image4.png");
-     let emplo5=  new employee(1004, 'Omar Zaid', 'Development', 'Senior',"https://exampl.com/image5.png");
-     let emplo6= new employee(1005, 'Rana Saleh', 'Development', 'Junior',"https://exampl.com/image6.png");
-     let emplo7= new employee(1006, 'Hadi Ahmad', 'Finance', 'Mid-Senior',"https://exampl.com/image7.png");
-    
+     let emplo1=   new employee( 'Ghazi Samer', 'Administration', 'Senior',"https://www.w3schools.com/howto/img_avatar2.png");
+     let emplo2= new employee( 'Lana Ali', 'Finance', 'Senior', "https://www.w3schools.com/howto/img_avatar2.png");
+     let emplo3=  new employee( 'Tamara Ayoub', 'Marketing', 'Senior',"https://www.w3schools.com/howto/img_avatar2.png");
+     let emplo4= new employee( 'Safi Walid', 'Administration', 'Mid-Senior',"https://www.w3schools.com/howto/img_avatar.png");
+     let emplo5=  new employee( 'Omar Zaid', 'Development', 'Senior',"https://www.w3schools.com/howto/img_avatar.png");
+     let emplo6= new employee( 'Rana Saleh', 'Development', 'Junior',"https://www.w3schools.com/howto/img_avatar2.png");
+     let emplo7= new employee('Hadi Ahmad', 'Finance', 'Mid-Senior',"https://www.w3schools.com/howto/img_avatar.png");
+
+    emplo1.card();
+    emplo2.card();
+    emplo3.card();
+    emplo4.card();
+    emplo5.card();
+    emplo6.card();
+    emplo7.card();
+   
 
 
 // for(let i=0;i< emplo.length; i++){
 //     emplo[i].calcSalary();
 // }
 
-emplo1.renderInfo();
-emplo2.renderInfo();
-emplo3.renderInfo();
-emplo4.renderInfo();
-emplo5.renderInfo();
-emplo6.renderInfo();
-emplo7.renderInfo();
+// emplo1.renderInfo();
+// emplo2.renderInfo();
+// emplo3.renderInfo();
+// emplo4.renderInfo();
+// emplo5.renderInfo();
+// emplo6.renderInfo();
+// emplo7.renderInfo();
